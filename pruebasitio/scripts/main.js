e5aca198b4584b2205d2113d3c485ca466b94006
MenuProject <113301364+MenuProject@users.noreply.github.com>
2022-09-11 10:08:22 -0600

    Add files via upload

---
+++ pruebasitio/scripts/main.js
@@ -0,0 +1,35 @@
+//Esto es un comentario
+/*
+Esto es un comentario 
+de varias líneas
+*/
+let miImage = document.querySelector('img');
+miImage.onclick = function () {
+    let miSrc = miImage.getAttribute('src');
+    if (miSrc === 'images/burguesa.jpg') {
+      miImage.setAttribute('src','images/Borgo.png');
+    } else {
+      miImage.setAttribute('src', 'images/burguesa.jpg');
+    }
+}
+let miBoton = document.querySelector('button');
+let miTitulo = document.querySelector( 'h1');
+function estableceNombreUsuario() {
+    let miNombre = prompt('Por favor, ingresa tu nombre.');
+    if(!miNombre) {
+        estableceNombreUsuario();
+      } else {
+        localStorage.setItem('nombre', miNombre);
+        miTitulo.innerHTML = 'Las hamburguesas son Geniales, ' + miNombre;
+      }
+}
+if (!localStorage.getItem('nombre')) {
+    estableceNombreUsuario();
+}
+else {
+    let nombreAlmacenado = localStorage.getItem('nombre');
+    miTitulo.textContent = 'Las hamburguesas son geniales,' + nombreAlmacenado;
+}
+miBoton.onclick = function() {
+    estableceNombreUsuario();
+}
